e17fe89bee731b3d045613db976ef28b5f23c6f1
cagatayyildiz <yildiz.cagatay@hotmail.com>
2024-02-10 11:43:06 +0300

    props sample

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import ProductBox from "./props/ProductBox";
+import UserDetail from "./props/UserDetail";
 
 function App() {
 
   return <>
   <ProductBox name="IPhone" unitPrice={5000} points={[3,5,4,7]}/>
+  <UserDetail name="Murat" surname="Yılmaz" age={20} />
 
   </>
 
---
+++ src/props/UserDetail.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function UserDetail(props) {
+
+    const { name, surname, age } = props
+
+    return (<>
+        <h1>Name: {name}</h1>
+        <h1>Surname: {surname}</h1>
+        <h1>Age: {age}</h1>
+    </>
+    )
+}
+
+export default UserDetail
+
+
